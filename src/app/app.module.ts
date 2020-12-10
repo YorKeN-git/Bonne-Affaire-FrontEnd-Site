446ff4754ae3aa12cf0294eb7927793ac96fcfc1
YorKeN <dethoor.axel@gmail.com>
2020-12-10 23:00:27 +0100

    Creation de carousel et acceuil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AcceuilComponent } from './pages/acceuil/acceuil.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AcceuilComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
